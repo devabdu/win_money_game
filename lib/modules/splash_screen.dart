bc4de277fe104119de055cd88e7154ef1ed78751
Abdel-rahmanalaa <Abdelrahmanalaa9911.com>
2022-07-27 00:44:34 +0200

    edit body

--- lib/modules/splash_screen.dart
+++ lib/modules/splash_screen.dart
@@ -11,40 +11,6 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       splash:Lottie.network('https://assets6.lottiefiles.com/packages/lf20_27klftwl.json'),
-    //   Column(
-    //   children: [
-    //     Image.asset(
-    //       'assets/images/logo_transparent.png',
-    //       width: 400,
-    //       height: 400,
-    //     ),
-    //     // Icon(
-    //     //   Icons.widgets_outlined,
-    //     //   size: 100,
-    //     //   color: Colors.white,
-    //     // ),
-    //     // SizedBox(
-    //     //   height: 10,
-    //     // ),
-    //     // Text(
-    //     //   'Jackpot',
-    //     //   style: TextStyle(
-    //     //     color: Colors.white,
-    //     //     fontSize: 35,
-    //     //     fontWeight: FontWeight.w600,
-    //     //     fontStyle: FontStyle.italic
-    //     //   ),
-    //     // ),
-    //     // Text(
-    //     //   'Win Real Cash',
-    //     //   style: TextStyle(
-    //     //     color: Colors.yellow,
-    //     //     fontSize: 25,
-    //     //     fontWeight: FontWeight.w300,
-    //     //   ),
-    //     // ),
-    //   ],
-    // ),
       backgroundColor: Colors.deepPurple,
       //animationDuration: Duration(seconds: 1),
       splashTransition: SplashTransition.fadeTransition,
