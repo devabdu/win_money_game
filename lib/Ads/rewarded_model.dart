d314a294160faa98f68b169eda92a99958b12219
mazenshawky <mazenshawqy@gmail.com>
2022-08-06 23:40:01 +0200

    test

--- lib/Ads/rewarded_model.dart
+++ lib/Ads/rewarded_model.dart
@@ -1,16 +1,16 @@
-import 'package:admob_flutter/admob_flutter.dart';
-import 'package:win_money_game/Ads/adsManager.dart';
-class AdRewarded{
-
-  late AdmobReward rewardAd;
-
-   void loadAd(){
-     rewardAd = AdmobReward(
-       adUnitId: AdsManager.rewardedAdUnitIdEx,
-       listener: (AdmobAdEvent event, Map<String, dynamic>? args) {
-         if (event == AdmobAdEvent.closed) rewardAd.load();
-       },
-     );
-     rewardAd.load();
-   }
-}
+// import 'package:admob_flutter/admob_flutter.dart';
+// import 'package:win_money_game/Ads/adsManager.dart';
+// class AdRewarded{
+//
+//   late AdmobReward rewardAd;
+//
+//    void loadAd(){
+//      rewardAd = AdmobReward(
+//        adUnitId: AdsManager.rewardedAdUnitIdEx,
+//        listener: (AdmobAdEvent event, Map<String, dynamic>? args) {
+//          if (event == AdmobAdEvent.closed) rewardAd.load();
+//        },
+//      );
+//      rewardAd.load();
+//    }
+// }
