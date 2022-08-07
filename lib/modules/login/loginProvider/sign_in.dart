1730a90ff7ceeeb982ca692fdffccf5e3c69a003
mazenshawky <mazenshawqy@gmail.com>
2022-08-07 16:24:21 +0200

    Mazen, database

--- lib/modules/login/loginProvider/sign_in.dart
+++ lib/modules/login/loginProvider/sign_in.dart
@@ -6,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:win_money_game/models/user_model.dart';
 
 class SignInProvider extends ChangeNotifier {
-  final googleSignIn = GoogleSignIn();
+  final googleSignIn = GoogleSignIn(scopes:['email']);
 
   bool aLoggedUser = false;
 
@@ -14,7 +14,7 @@ class SignInProvider extends ChangeNotifier {
 
   GoogleSignInAccount get user => _user!;
 
-  Future googleLogin() async {
+  Future googleLogin(context) async {
     try {
       final googleUser = await googleSignIn.signIn();
       if (googleUser == null) return;
@@ -29,33 +29,71 @@ class SignInProvider extends ChangeNotifier {
 
       aLoggedUser = false;
 
-      FirebaseAuth.instance.signInWithCredential(credential).then((value)
-      async {
-        await FirebaseFirestore.instance
-            .collection('users')
-            .get()
-            .then((QuerySnapshot querySnapshot) {
-          querySnapshot.docs.forEach((doc) {
-            if(value.user!.email.toString() == doc["email"])
-              aLoggedUser = true;
-          });
-        });
+      await FirebaseAuth.instance.signInWithCredential(credential);
 
-        if(!aLoggedUser)
-        {
-          userSignUp(
-            name: value.user!.displayName.toString(),
-            email: value.user!.email.toString(),
-            uId: value.user!.uid,
-          );
-        }
-      }).catchError((error){
-        print(error.toString());
+      await FirebaseFirestore.instance
+          .collection('users')
+          .get()
+          .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) {
+          if(googleUser.email == doc["email"])
+            aLoggedUser = true;
+        });
       });
-    } catch (error) {
+
+      if(!aLoggedUser)
+      {
+        userSignUp(
+          name: googleUser.displayName.toString(),
+          email: googleUser.email,
+          uId: googleUser.id,
+        );
+      }
+    } on FirebaseAuthException catch (error){
       print(error.toString());
+
+      var content = '';
+      switch(error.code) {
+        case 'account-exists-with-different-credential':
+          content = 'This account exists with a different sign in provider';
+          break;
+        case 'invalid-credential':
+          content = 'Unknown error has occurred';
+          break;
+        case 'operation-not-allowed':
+          content = 'This operation is not allowed';
+          break;
+        case 'user-disabled':
+          content = 'The user you tried to log into is disabled';
+          break;
+        case 'user-not-found':
+          content = 'The user you tried to log into was not found';
+          break;
+      }
+      showDialog(context: context, builder: (context) => AlertDialog(
+        title: const Text('log in with google failed'),
+        content: Text(content),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop();
+          }, child: const Text('Ok'),
+          ),
+        ],
+      ));
+      notifyListeners();
+    } catch (e) {
+      showDialog(context: context, builder: (context) => AlertDialog(
+        title: const Text('log in with google failed'),
+        content: const Text('An unknown error occurred'),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.of(context).pop();
+          }, child: const Text('Ok'),
+          ),
+        ],
+      ));
     }
-    notifyListeners();
+
   }
 
   Future googleLogout() async {
@@ -71,7 +109,7 @@ class SignInProvider extends ChangeNotifier {
 
       aLoggedUser = false;
 
-      FirebaseAuth.instance.signInWithCredential(credential).then((value)
+      await FirebaseAuth.instance.signInWithCredential(credential).then((value)
       async {
         await FirebaseFirestore.instance
             .collection('users')
@@ -139,7 +177,7 @@ class SignInProvider extends ChangeNotifier {
     required String name,
     required String email,
     required String uId,
-}) {
+}) async {
     UserModel userModel = UserModel(
       name: name,
       email: email,
@@ -150,7 +188,7 @@ class SignInProvider extends ChangeNotifier {
       exp: 0,
       avatar: 7,
     );
-    FirebaseFirestore.instance
+    await FirebaseFirestore.instance
         .collection('users')
         .doc(uId)
         .set(userModel.toMap())
--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -71,7 +71,7 @@ class LoginScreen extends StatelessWidget {
                     onPressed: () {
                       final provider = Provider.of<SignInProvider>(
                           context, listen: false);
-                      provider.googleLogin();
+                      provider.googleLogin(context);
                     },
                   ),
                 ],
