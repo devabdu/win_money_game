eaaafb51832f527dbac9b5d569611bc73a7dc2fb
Abdel-rahmanalaa <Abdelrahmanalaa9911.com>
2022-08-24 22:48:12 +0200

    (abdo) new commit

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -76,6 +76,7 @@ class LoginScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              scrollDirection: Axis.vertical,
             ),
           );
         }
